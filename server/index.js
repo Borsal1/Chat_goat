59e460dfdc03d879fa3b77fd05fe6a24268f6715
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2023-12-10 22:53:03 +0100

    Réecriture Backend

--- server/index.js
+++ server/index.js
@@ -61,13 +61,125 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Erreur serveur' });
 });
 
+app.post('/sendMsg', (req, res) => {
+  const data = req.body;
+  const content = data.content;
+  const sender = parseInt(data.sender);
+  const receiver = parseInt(data.receiver);
+  const timestamp = new Date().toISOString();
+
+  // Insérer le message dans la base de données
+  const query = "INSERT INTO message (content, date, senderId, receiverId) VALUES ("+content+", "+timestamp+", "+sender+", "+receiver+")";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Erreur lors de l\'envoi du message : ' + err.message);
+        res.status(500).json({ error: 'Erreur serveur' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.post('/addUser', (req, res) => {
+  const data = req.body;
+  const name = data.name;
+  const firstName = data.firstName;
+  const email = data.email;
+  const password = data.password;
+  const query = "INSERT INTO userChat (name, first_name, email, password) VALUES ("+name+", "+firstName+", "+email+", "+password+")";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Erreur lors de la création de l\'utilisateur : ' + err.message);
+        res.status(500).json({ error: 'Erreur serveur' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 // Récupération des 10 derniers messages
 app.get('/messages', (req, res) => {
-  const query = 'SELECT * FROM messages ORDER BY timestamp DESC LIMIT 10';
+  const query = 'SELECT * FROM message ORDER BY date DESC LIMIT 10';
 
   db.query(query, (err, results) => {
     if (err) {
       console.error('Erreur lors de la récupération des messages : ' + err.message);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/messages/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const receiverId = parseInt(req.body.receiver);
+  const query = 'SELECT * FROM message WHERE senderId = '+userId+' AND receiverId = '+receiverId+' ORDER BY date DESC LIMIT 10';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Erreur lors de la récupération des messages : ' + err.message);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/users', (req, res) => {
+  const query = 'SELECT * FROM userChat ORDER BY id';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Erreur lors de la récupération des utilisateurs : ' + err.message);
+      res.status(500).json({ error: 'Erreur serveur' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const query = 'SELECT * FROM userChat WHERE id = '+userId;
+
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Erreur lors de la récupération de l\'utilisateur : ' + err.message);
+        res.status(500).json({ error: 'Erreur serveur' });
+      } else {
+        res.json(results);
+      }
+    });
+});
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const data = req.body;
+    const name = data.name;
+    const firstName = data.firstName;
+    const email = data.email;
+    const query = "UPDATE userChat SET name = "+name+", first_name = "+firstName+", email = "+email+" WHERE id = "+userId;
+
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Erreur lors de la modification de l\'utilisateur : ' + err.message);
+        res.status(500).json({ error: 'Erreur serveur' });
+      } else {
+        res.json(results);
+      }
+    });
+});
+
+app.delete('/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  const query = "DELETE FROM userChat WHERE id = "+userId;
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Erreur lors de la suppression de l\'utilisateur : ' + err.message);
       res.status(500).json({ error: 'Erreur serveur' });
     } else {
       res.json(results);
